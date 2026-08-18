7db7c8b0551bf3ba83c4e512cc3ce312f90d2f0d
OmMaheshwari653 <ommaheshwari869@gmail.com>
2026-08-18 12:47:17 +0530

    Created Frontened of Credentials

--- src/app/(dashboard)/(rest)/credentials/[credentialid]/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/[credentialid]/page.tsx
@@ -1,12 +1,39 @@
+import { CredentialView } from "@/features/credentials/components/Credential";
+import {
+  CredentialsError,
+  CredentialsLoading,
+} from "@/features/credentials/components/Credentials";
+import { prefetchCredential } from "@/features/credentials/server/prefetch";
+import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 interface PageProps {
-  params: {
-    credentialid: string;
-  };
+  params: Promise<{
+    credentialId: string;
+  }>;
 }
 
-const page = async ({ params }: PageProps) => {
-  const { credentialid } = await params;
-  return <div> credentialid: {credentialid} </div>;
+const Page = async ({ params }: PageProps) => {
+  await requireAuth();
+
+  const { credentialId } = await params;
+  prefetchCredential(credentialId);
+
+  return (
+    <div className="p-4 md:px-10 md:py-6 h-full">
+      <div className="mx-auto max-w-screen-md w-full flex flex-col gap-y-8 h-full">
+        <HydrateClient>
+          <ErrorBoundary fallback={<CredentialsError />}>
+            <Suspense fallback={<CredentialsLoading />}>
+              <CredentialView credentialId={credentialId} />
+            </Suspense>
+          </ErrorBoundary>
+        </HydrateClient>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
